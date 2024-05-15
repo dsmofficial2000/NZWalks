1c38ac1f5342c5db4d5616ffed99ea8c13807526
Durgesh Marathe <dsmofficial2000@gmail.com>
2024-05-16 00:31:16 +0530

    Add_ConnString&Db

---
+++ NZWalks.API/Data/NzWalksDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.Models.Domain;
+using System.Runtime.Versioning;
+
+namespace NZWalks.Data
+{
+    public class NzWalksDBContext : DbContext
+    {
+        public NzWalksDBContext(DbContextOptions dbContextOptions) : base (dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> difficulties{ get; set; }
+        public DbSet<Region> regions { get; set; }  
+        public DbSet<Walk> walks { get; set; }
+    }
+}

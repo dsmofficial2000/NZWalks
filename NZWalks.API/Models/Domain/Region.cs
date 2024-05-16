24264690bbb2f8bf9e5dfdf5cee041ff7d19baaa
Durgesh Marathe <dsmofficial2000@gmail.com>
2024-05-17 00:11:39 +0530

    Add Region model controller

--- NZWalks.API/Models/Domain/Region.cs
+++ NZWalks.API/Models/Domain/Region.cs
@@ -3,7 +3,7 @@
     public class Region
     {
         public Guid Id { get; set; }
-        public string Coode { get; set; }
+        public string Code { get; set; }
         public string Name { get; set; }
         public string? RegionImageUrl { get; set; }
     }
